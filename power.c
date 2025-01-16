1a9d96c162cbb5b310940cc6d79a31b8e138e445
Stewart Almeida <salmeida1993@login-el8.khoury.northeastern.edu>
2025-01-16 14:56:16 -0500

    v2

--- power.c
+++ power.c
@@ -5,7 +5,7 @@ long power(int base, unsigned int n);
 int main() {
 	for (int x = 1; x <= 10; x++) {
 		long answer = power(2, x);
-		printf("2 to the power of %d is %ld\n", x, answer);
+		printf("2 to the power of %d is: %ld\n", x, answer);
 	}
 	return 0;
 }
